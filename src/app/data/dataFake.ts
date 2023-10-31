10713a4ad119f181dbfa2db5a3152d31ccd432c1
Matheus Pires <mattpires@icloud.com>
2023-10-31 19:07:05 -0300

    feat: adiciona modificacoes

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "NOVO HR-V",
+    "description": "Se você está buscando um novo carro, não se arrisque e compre na Localiza Seminovos: carros com 360 itens verificados, garantia e procedência. Aqui você encontra a maior variedade de modelos do mercado, condições únicas de financiamento, entrada facilitada em até 10 vezes sem juros, carros revisados e com garantia de quilometragem real. ",
+    "photoCover":"https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202310/20231014/honda-hrv-1.8-16v-flex-lx-4p-automatico-wmimagem09483977418.jpg?s=fill&w=1920&h=1440&q=75"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "NOVO CRETA",
+    "description": " Se você está buscando um novo carro, não se arrisque e compre na Localiza Seminovos: carros com 360 itens verificados, garantia e procedência. Aqui você encontra a maior variedade de modelos do mercado, condições únicas de financiamento, entrada facilitada em até 10 vezes sem juros, carros revisados e com garantia de quilometragem real. ",
+    "photoCover":"https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202310/20231019/hyundai-creta-1.6-16v-flex-action-automatico-wmimagem13124541136.jpg?s=fill&w=1920&h=1440&q=75"
+  },
+  {
+    "id":"3",
+    "title": "NOVO T-CROSS",
+    "description": "Se você está buscando um novo carro, não se arrisque e compre na Localiza Seminovos: carros com 360 itens verificados, garantia e procedência. Aqui você encontra a maior variedade de modelos do mercado, condições únicas de financiamento, entrada facilitada em até 10 vezes sem juros, carros revisados e com garantia de quilometragem real. ",
+    "photoCover":"https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202309/20230930/volkswagen-tcross-1.4-250-tsi-total-flex-highline-automatico-wmimagem1329418743.jpg?s=fill&w=1920&h=1440&q=75"
+  },
+  {
+    "id":"4",
+    "title": "NOVA TRACKER",
+    "description": "Se você está buscando um novo carro, não se arrisque e compre na Localiza Seminovos: carros com 360 itens verificados, garantia e procedência. Aqui você encontra a maior variedade de modelos do mercado, condições únicas de financiamento, entrada facilitada em até 10 vezes sem juros, carros revisados e com garantia de quilometragem real. ",
+    "photoCover":"https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202310/20231026/chevrolet-tracker-1.2-turbo-flex-premier-automatico-wmimagem18182476417.jpg?s=fill&w=1920&h=1440&q=75"
   }
 ]
